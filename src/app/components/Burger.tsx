4eae2c6736e431670fff3f81e35f5584e8dad267
DBryja <xileran@gmail.com>
2024-03-13 18:18:29 +0100

    Update burger menu

--- src/app/components/Burger.tsx
+++ src/app/components/Burger.tsx
@@ -19,25 +19,25 @@ export default function Burger() {
         <>
         <button ref={buttonRef} className="max-sm:block md:hidden relative group z-20" onClick={() => setOpen((open)=>!open)}>
             <div
-                className={"flex flex-col justify-between w-[25px] h-[25px] transform transition-all duration-300 origin-center overflow-hidden"}>
+                className={"flex flex-col justify-between w-[25px] h-[25px] transform transition-all duration-200 origin-center overflow-hidden"}>
                 <div
-                    className={`bg-beige h-[2px] w-8 transform transition-all duration-300 origin-left ${open && "translate-x-10"} `}/>
+                    className={`bg-beige h-[2px] w-8 transform transition-all duration-200 origin-left ${open && "translate-x-10"} `}/>
                 <div
-                    className={`bg-beige h-[2px] w-8 rounded transform transition-all duration-300 ${open && "translate-x-10"}  delay-75`}/>
+                    className={`bg-beige h-[2px] w-8 rounded transform transition-all duration-200 ${open && "translate-x-10"}  delay-50`}/>
                 <div
-                    className={`bg-beige h-[2px] w-8 transform transition-all duration-300 origin-left ${open && "translate-x-10"}  delay-150`}/>
+                    className={`bg-beige h-[2px] w-8 transform transition-all duration-300 origin-left ${open && "translate-x-10"}  delay-100`}/>
                 <div className={`absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 ${open && "translate-x-0"} flex w-0 ${open && "w-12"}`}>
                     <div
-                        className={`absolute bg-beige h-[2px] w-7 rotate-0 transform transition-all duration-500 ${open && "rotate-45"}  delay-300`}/>
+                        className={`absolute bg-beige h-[2px] w-7 rotate-0 transform transition-all duration-500 ${open && "rotate-45"}  delay-200`}/>
                     <div
-                        className={`bg-beige h-[2px] w-7 -rotate-0 transform transition-all duration-500 ${open && "-rotate-45"}  delay-300`}/>
+                        className={`bg-beige h-[2px] w-7 -rotate-0 transform transition-all duration-500 ${open && "-rotate-45"}  delay-200`}/>
                 </div>
             </div>
         </button>
-            <nav ref={navRef} className={`max-sm:flex md:hidden absolute z-10 right-0 top-0 h-screen ${open && "translate-x-0 shadow-lg drop-shadow-xl" || "translate-x-full"} flex-col px-8 py-12 w-3/4 transform transition-transform overflow-x-hidden text-2xl  text-right gap-y-8 bg-[rgba(0,0,0,0.5)] backdrop-blur-2xl`}>
-                <span className={"flex justify-end items-center gap-x-2 mt-4"}>Services<FaWrench/></span>
-                <span className={"flex justify-end items-center gap-x-2"}>Demos<FaCamera/></span>
-                <span className={"flex justify-end items-center gap-x-2"}>About Us<FaPeopleGroup/></span>
+            <nav ref={navRef} className={`max-sm:flex md:hidden absolute z-10 right-0 top-0 h-min rounded-b-3xl w-full ${open && "translate-y-0 shadow-lg drop-shadow-xl" || "-translate-y-full"} flex-col px-8 py-12 w-3/4 transform transition-transform duration-300 overflow-x-hidden text-3xl  text-right gap-y-8 bg-black`}>
+                <span className={"flex hover-orange justify-between items-center gap-x-2 mt-4"}>Services<FaWrench/></span>
+                <span className={"flex hover-orange justify-between items-center gap-x-2"}>Demos<FaCamera/></span>
+                <span className={"flex hover-orange justify-between items-center gap-x-2"}>About Us<FaPeopleGroup/></span>
             </nav>
         </>
     )
